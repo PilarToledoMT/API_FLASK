7288f3acc294d5c64fec8f4386062eec2d7ee531
Leonardo Villegas Costa <leonardovillegascosta@gmail.com>
2023-09-15 08:57:45 -0300

    Creada la primera rama con la estructura del proyecto

---
+++ api/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from config import Config
+from .database import DatabaseConnection
+
+def init_app():
+    app = Flask (__name__, static_folder = Config.STATIC_FOLDER, template_folder = Config.TEMPLATE_FOLDER)
+    
+    app.config.from_object(Config)
+    
+    return app
--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-#esto es un comentario
+from dotenv import dotenv_values
+
+class Config:
+    config = dotenv_values(".env")
+    
+    SERVER_NAME = "127.0.0.1:5000"
+    DEBUG = True
+    
+    DATABASE_USERNAME = config['DATABASE_USERNAME']
+    DATABASE_PASSWORD = config['DATABASE_PASSWORD']
+    DATABASE_HOST = config['DATABASE_HOST']
+    DATABASE_PORT = config['DATABASE_PORT']
+    
+    TEMPLATE_FOLDER = "templates/"
+    STATIC_FOLDER = "static_folder/"
