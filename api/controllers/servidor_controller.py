4e89c8447473e7e452878a2ad6ecf89725b50068
unknown <raffalopez@gmail.com>
2023-09-24 14:55:51 -0300

    servidor con error

---
+++ api/controllers/servidor_controller.py
@@ -0,0 +1,17 @@
+from ..models.servidor_model import ServidorModel
+from flask import request,jsonify
+
+class ServidorController:
+
+    @classmethod
+    def get_server_controller(cls,id_servidor):
+        server_instance = ServidorModel.get_server_model(id_servidor)
+        if server_instance:
+            response_data = {
+                'id_servidor':server_instance.id_servidor,
+                'nombre_servidor':server_instance.nombre_servidor,
+                'imagen_servidor':server_instance.imagen_servidor
+            }
+            return jsonify(response_data), 200
+        else:
+            return {'msg': 'No se encontró el producto'}, 404
---
+++ api/models/servidor_model.py
@@ -0,0 +1,57 @@
+from ..database import DatabaseConnection
+
+class ServidorModel:
+    def __init__ (self, nombre_servidor=None, id_servidor=None, imagen_servidor=None):
+        self.nombre_servidor = nombre_servidor
+        self.id_servidor = id_servidor
+        self.imagen_servidor = imagen_servidor
+
+    @classmethod
+    def get_server_model (cls, id_servidor):
+        query = "SELECT servidores.id_servidor, servidores.nombre_servidor, servidores.imagen_servidor FROM chat_master.servidores WHERE servidores.id_servidor=%s;" 
+        params = id_servidor,
+        result = DatabaseConnection.fetch_one(query,params)
+        if result is not None:
+            return ServidorModel(
+                id_servidor=result[0],
+                nombre_servidor=result[1],
+                imagen_servidor=result[2]
+            )
+        else:
+            return None
+    
+    '''@classmethod
+    def get_all_servers (cls):
+        query = "SELECT servidores.id_servidor, servidores.nombre_servidor, servidores.imagen_servidor FROM chat_master.servidores;"
+        result = DatabaseConnection.fetch_all(query)
+        if result is not None:
+            return result
+        return None
+
+    @classmethod
+    def exists(cls, servidor):
+        query = "SELECT servidores.nombre_servidor FROM chat_master.servidores WHERE servidores.id_servidor=%s;"
+        params = servidor,
+        result = DatabaseConnection.fetch_one(query, params)
+        if result is not None:
+            return True
+        return False
+
+    @classmethod
+    def create_server(cls, servidor):
+        query = "INSERT INTO chat_master.servidores (servidores.nombre_servidor, servidores.imagen_servidor) VALUES (%s,%s);"
+        params = servidor, 
+        DatabaseConnection.execute_query(query, params)
+
+    @classmethod
+    def change_server_name(cls, servidor):
+        query = "UPDATE chat_master.servidores SET servidores.nombre_servidor WHERE servidores.id_servidor=%s;"
+        params = servidor,
+        DatabaseConnection.execute_query(query,params)
+
+    @classmethod
+    def delete_server (cls, servidor):
+        query = "DELETE FROM chat_master.servidores WHERE servidores.id_servidor = %s"
+        params = servidor,
+        DatabaseConnection.execute_query(query, params)
+    '''
