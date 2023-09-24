ba8211f6a6abaa3d44e43908feb0c18ce8b222b5
María Dolores Costa <mdcostab@gmail.com>
2023-09-24 18:17:48 -0300

    agrego metodos al modelo imagen_perfil, creo su controlador y rutas (no funciona el update)

--- api/__init__.py
+++ api/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from .database import DatabaseConnection
 from .routes.usuarios_bp import usuarios_bp
+from .routes.imagen_perfil_bp import imagen_perfil_bp
 
 def init_app():
     app = Flask (__name__, static_folder = Config.STATIC_FOLDER, template_folder = Config.TEMPLATE_FOLDER)
@@ -9,6 +10,7 @@ def init_app():
     app.config.from_object(Config)
     DatabaseConnection.set_config(app.config)
     app.register_blueprint(usuarios_bp)
+    app.register_blueprint(imagen_perfil_bp)
 
     
     return app
