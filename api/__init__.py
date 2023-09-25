75ac9031e05f1921e58e06969d54cadb0a6baa3d
Leonardo Villegas Costa <leonardovillegascosta@gmail.com>
2023-09-25 20:30:23 -0300

    agrego la ruta de mensajes_bp al __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from .database import DatabaseConnection
 from .routes.usuarios_bp import usuarios_bp
 from .routes.imagen_perfil_bp import imagen_perfil_bp
 from .routes.servidores_bp import servidores_bp
+from .routes.mensajes_bp import mensajes_bp
 
 def init_app():
     app = Flask (__name__, static_folder = Config.STATIC_FOLDER, template_folder = Config.TEMPLATE_FOLDER)
@@ -13,6 +14,7 @@ def init_app():
     app.register_blueprint(usuarios_bp)
     app.register_blueprint(imagen_perfil_bp)
     app.register_blueprint(servidores_bp)
+    app.register_blueprint(mensajes_bp)
 
     
     return app
