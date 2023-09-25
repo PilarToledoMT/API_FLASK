ae0d5f8b56423e85f039c97c8497f0bb4cfd0882
Leonardo Villegas Costa <leonardovillegascosta@gmail.com>
2023-09-25 20:04:18 -0300

    agrego las rutas de servidores al __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from .database import DatabaseConnection
 from .routes.usuarios_bp import usuarios_bp
 from .routes.imagen_perfil_bp import imagen_perfil_bp
+from .routes.servidores_bp import servidores_bp
 
 def init_app():
     app = Flask (__name__, static_folder = Config.STATIC_FOLDER, template_folder = Config.TEMPLATE_FOLDER)
@@ -11,6 +12,7 @@ def init_app():
     DatabaseConnection.set_config(app.config)
     app.register_blueprint(usuarios_bp)
     app.register_blueprint(imagen_perfil_bp)
+    app.register_blueprint(servidores_bp)
 
     
     return app
