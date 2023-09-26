666934792baf826bbd371440e8f04796fbff2a91
Pilar Toledo <plrtoledo@gmail.com>
2023-09-26 10:05:47 -0300

    Prueba

--- api/__init__.py
+++ api/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from config import Config
 from .database import DatabaseConnection
+from .routes.usuarios_bp import usuarios_bp
+from .routes.imagen_perfil_bp import imagen_perfil_bp
+from .routes.servidores_bp import servidores_bp
+from .routes.mensajes_bp import mensajes_bp
 
 def init_app():
     app = Flask (__name__, static_folder = Config.STATIC_FOLDER, template_folder = Config.TEMPLATE_FOLDER)
+    
     app.config.from_object(Config)
     DatabaseConnection.set_config(app.config)
     app.register_blueprint(usuarios_bp)
@@ -12,5 +17,6 @@ def init_app():
     app.register_blueprint(servidores_bp)
     app.register_blueprint(mensajes_bp)
 
+    CORS(app)
     
     return app
